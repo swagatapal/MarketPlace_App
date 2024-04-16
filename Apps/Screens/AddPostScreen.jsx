c7eedb559e952a8ff10f6d711b1c5fdde9df2614
Swagata Pal <swagata.pal@blucocoondigital.com>
2024-04-16 23:40:46 +0530

    firebase data fetch

--- Apps/Screens/AddPostScreen.jsx
+++ Apps/Screens/AddPostScreen.jsx
@@ -1,7 +1,28 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import { app } from '../../firebase_config';
+import { getFirestore, getDocs,collection  } from "firebase/firestore";
+import React, { useState, useEffect } from 'react';
 
 export default function AddPostScreen() {
+
+// fetch the category list from firebase
+const db = getFirestore(app);
+useEffect(()=>{
+  getCategoryList();  
+
+},[])
+
+const getCategoryList=async ()=>{
+
+      const querySnapshot = await getDocs(collection(db, "category"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+
+
+}
+
   return (
     <View>
       <Text>AddPostScreen</Text>
